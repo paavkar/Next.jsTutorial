abe69c018f3755d4646fe345607dbe0a1a8a2bd7
Paavo Karppinen <paavo_karppinen@outlook.com>
2024-05-19 14:05:58 +0300

    Finished tutorial (chapters 13-16)

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <Table customers={customers} />
+        </div>
+    )
 }
